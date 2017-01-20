7d9c0674b3467b32c5d4070406cc90f2e154ec33
JohnRoz <yonimeiron99@gmail.com>
2017-01-20 12:19:14 +0200

    Starting to work...
    Added 2 Buttons, background...
    TO DO:
    more activities, Fragments etc

---
+++ Absolute/app/src/main/java/com/example/user1/absolute/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.user1.absolute;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Random;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        for(int i = 0;i<11;i++){
+            Random random = new Random();
+            int index = random.nextInt(getRawResourcesIds().size());
+            int resId = getRawResourcesIds().get(index);
+            Log.d("TAG!!", "resId: " + resId);
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), resId);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                    mp = null;
+                }
+            });
+        }
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Random random = new Random();
+                int index = random.nextInt(getRawResourcesIds().size());
+                int resId = getRawResourcesIds().get(index);
+                Log.d("TAG!!", "resId: " + resId);
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), resId);
+                mp.start();
+                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                        mp = null;
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * The sinner function (but it's a necessary sin).
+     *
+     * @return The ArrayList of the names of the resources in R.raw .
+     */
+    private ArrayList<String> getRawResourcesNames() {
+
+        //an ArrayList to contain the names of the resources of raw.
+        //This is the ArrayList to be returned.
+        ArrayList<String> rawResourcesNames = new ArrayList<>();
+
+        // (I THINK! = NOT SURE) this is an array of copies of the resources of the raw class
+        final Field[] fields = R.raw.class.getDeclaredFields();
+
+        //the loop runs as the length of the 'fields' array -
+        // meaning, as the number of the resources raw class has in it
+        for (Field field : fields) {
+            final String resourceName;
+            //the name of the current resource
+            resourceName = field.getName();
+
+            //if the id of the resource isn't 0 (if it is it brings up problems)
+            if (getApplicationContext().getResources().getIdentifier(resourceName, "raw", "com.example.user1.absolute") != 0)
+                //the loop adds each name of each of the resources of raw to an ArrayList
+                rawResourcesNames.add(resourceName);
+
+
+        }
+
+        //The list of names of the resources of the raw class is being returned
+        return rawResourcesNames;
+    }
+
+    private ArrayList<Integer> getRawResourcesIds(){
+        ArrayList<Integer> IDs = new ArrayList<>();
+        for (String name : getRawResourcesNames()){
+            int id = getApplicationContext().getResources().getIdentifier(name,"raw", "com.example.user1.absolute");
+            IDs.add(id);
+        }
+        return IDs;
+    }
+}
